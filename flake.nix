ea210145547020f2630879b3b197bcf7eaeb31ee
Cole Stowell <cole@stowell.pro>
2025-09-20 19:39:00 -0400

    init: initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "ZFS NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs { inherit system; };
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      reef = lib.nixosSystem {
+        modules = [./hosts/reef];
+      }
+    }
+  }
+}
